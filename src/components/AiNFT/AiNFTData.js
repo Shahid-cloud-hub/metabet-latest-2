a33cabc62b7025c60f04d9f095c40a865d2f1756
sajiha77 <buttliaqat7@gmail.com>
2023-04-13 13:11:28 +0500

    update

--- src/components/AiNFT/AiNFTData.js
+++ src/components/AiNFT/AiNFTData.js
@@ -42,6 +42,52 @@ import Dwayne from "../../assets/images/Ai-NFT/Dwayne.png";
 import Harry from "../../assets/images/Ai-NFT/Harry.png";
 import Mukesh from "../../assets/images/Ai-NFT/Mukesh.png";
 
+//leaderboard images
+
+import cristiano from "../../assets/images/Ai-NFT/Leaderboard/Cristiano.png";
+import lionel from "../../assets/images/Ai-NFT/Leaderboard/lionel.png";
+import kim from "../../assets/images/Ai-NFT/Leaderboard/kim.png";
+import dwayne from "../../assets/images/Ai-NFT/Leaderboard/dwayne.svg";
+import beyonce from "../../assets/images/Ai-NFT/Leaderboard/beyonce.svg";
+import elon from "../../assets/images/Ai-NFT/Leaderboard/elon.svg";
+import barak from "../../assets/images/Ai-NFT/Leaderboard/barak.svg";
+import bill from "../../assets/images/Ai-NFT/Leaderboard/bill.svg";
+import bernard from "../../assets/images/Ai-NFT/Leaderboard/bernard.svg";
+import lebron from "../../assets/images/Ai-NFT/Leaderboard/lebron.svg";
+import virat from "../../assets/images/Ai-NFT/Leaderboard/virat.svg";
+import donald from "../../assets/images/Ai-NFT/Leaderboard/donald.svg";
+import jeff from "../../assets/images/Ai-NFT/Leaderboard/jeff.svg";
+import mukesh from "../../assets/images/Ai-NFT/Leaderboard/mukesh.svg";
+import connor from "../../assets/images/Ai-NFT/Leaderboard/connor.svg";
+import joe from "../../assets/images/Ai-NFT/Leaderboard/joe.svg";
+import gautam from "../../assets/images/Ai-NFT/Leaderboard/gautam.svg";
+import arnold from "../../assets/images/Ai-NFT/Leaderboard/arnold.svg";
+import floyd from "../../assets/images/Ai-NFT/Leaderboard/floyd.svg";
+import rafael from "../../assets/images/Ai-NFT/Leaderboard/rafael.svg";
+import mike from "../../assets/images/Ai-NFT/Leaderboard/mike.svg";
+import lewis from "../../assets/images/Ai-NFT/Leaderboard/lewis.svg";
+import michael from "../../assets/images/Ai-NFT/Leaderboard/michael.svg";
+import khabib from "../../assets/images/Ai-NFT/Leaderboard/khabib.svg";
+import usain from "../../assets/images/Ai-NFT/Leaderboard/usain.svg";
+import roger from "../../assets/images/Ai-NFT/Leaderboard/roger.svg";
+import serena from "../../assets/images/Ai-NFT/Leaderboard/serena.svg";
+import jake from "../../assets/images/Ai-NFT/Leaderboard/jake.svg";
+import novak from "../../assets/images/Ai-NFT/Leaderboard/novak.svg";
+import sylvester from "../../assets/images/Ai-NFT/Leaderboard/sylvester.svg";
+import tom from "../../assets/images/Ai-NFT/Leaderboard/tom.svg";
+import canelo from "../../assets/images/Ai-NFT/Leaderboard/canelo.svg";
+import volodymyr from "../../assets/images/Ai-NFT/Leaderboard/volodymyr.svg";
+import changpeng from "../../assets/images/Ai-NFT/Leaderboard/chengpeng.svg";
+import kanye from "../../assets/images/Ai-NFT/Leaderboard/kanye.svg";
+import dana from "../../assets/images/Ai-NFT/Leaderboard/dana.svg";
+import tiger from "../../assets/images/Ai-NFT/Leaderboard/tiger.svg";
+import tyson from "../../assets/images/Ai-NFT/Leaderboard/tyson.svg";
+import gervonta from "../../assets/images/Ai-NFT/Leaderboard/Gervonta.svg";
+import vladimir from "../../assets/images/Ai-NFT/Leaderboard/vladimir.svg";
+import janet from "../../assets/images/Ai-NFT/Leaderboard/janet.svg";
+import christine from "../../assets/images/Ai-NFT/Leaderboard/christine.svg";
+import harry from "../../assets/images/Ai-NFT/Leaderboard/harry.svg";
+
 export const NftData = [
   {
     id: 1,
@@ -908,5 +954,377 @@ export const NftData = [
     wallet_id1: "be23x......326bdg2v",
     price_2: "12.00",
     price_3: "10.00",
+  },
+];
+
+export const Leaderboard = [
+  {
+    id: 1,
+    number: "№4",
+    name: "Dwayne The Rock Johnson ",
+    img: dwayne,
+    score: "453",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 2,
+    number: "№5",
+    name: "Beyonce",
+    img: beyonce,
+    score: "376",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 3,
+    number: "№6",
+    name: "Elon Musk",
+    img: elon,
+    score: "329",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 4,
+    number: "№7",
+    name: "Barak Obama",
+    img: barak,
+    score: "225",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 5,
+    number: "№8",
+    name: "Bill Gates",
+    img: bill,
+    score: "221",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 6,
+    number: "№9",
+    name: "Bernard Arnault",
+    img: bernard,
+    score: "221",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 7,
+    number: "№10",
+    name: "LeBron James",
+    img: lebron,
+    score: "218",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 8,
+    number: "№11",
+    name: "Virat Kohli",
+    img: virat,
+    score: "170",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 9,
+    number: "№12",
+    name: "Donald Trump",
+    img: donald,
+    score: "149",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 10,
+    number: "№13",
+    name: "Jeff Bezos",
+    img: jeff,
+    score: "132",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 11,
+    number: "№14",
+    name: "Jeff Bezos",
+    img: jeff,
+    score: "132",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 12,
+    number: "№15",
+    name: "Mukesh Ambani",
+    img: mukesh,
+    score: "85",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 13,
+    number: "№16",
+    name: "Connor McGregor",
+    img: connor,
+    score: "72",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 14,
+    number: "№17",
+    name: "Joe Biden",
+    img: joe,
+    score: "66",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 15,
+    number: "№18",
+    name: "Gautam Adani",
+    img: gautam,
+    score: "59",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 16,
+    number: "№19",
+    name: "Arnold Schwarzenegger",
+    img: arnold,
+    score: "53",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 17,
+    number: "№20",
+    name: "Floyd Mayweather Jr",
+    img: floyd,
+    score: "53",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 18,
+    number: "№21",
+    name: "Rafael Nadal",
+    img: rafael,
+    score: "50",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 19,
+    number: "№22",
+    name: "Mike Tyson",
+    img: mike,
+    score: "49",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 20,
+    number: "№23",
+    name: "Lewis Hamilton",
+    img: lewis,
+    score: "45",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 21,
+    number: "№24",
+    name: "Michael Jordan",
+    img: michael,
+    score: "43",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 22,
+    number: "№25",
+    name: "Khabib Nurmagomedov",
+    img: khabib,
+    score: "43",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 23,
+    number: "№26",
+    name: "Usain Bolt",
+    img: usain,
+    score: "39",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 24,
+    number: "№27",
+    name: "Roger Federer",
+    img: roger,
+    score: "38",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 25,
+    number: "№28",
+    name: "Serena Williams",
+    img: serena,
+    score: "36",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 26,
+    number: "№29",
+    name: "Jake Paul",
+    img: jake,
+    score: "34",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 27,
+    number: "№30",
+    name: "Novak Djokovic",
+    img: novak,
+    score: "34",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 28,
+    number: "№31",
+    name: "Sylvester Stallone",
+    img: sylvester,
+    score: "31",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 29,
+    number: "№32",
+    name: "Tom Brady",
+    img: tom,
+    score: "26",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 30,
+    number: "№33",
+    name: "Canelo Álvarez",
+    img: canelo,
+    score: "25",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 31,
+    number: "№34",
+    name: "Volodymyr Zelenskyy",
+    img: volodymyr,
+    score: "24",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 32,
+    number: "№35",
+    name: "ChangPeng Zhao",
+    img: changpeng,
+    score: "20",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 33,
+    number: "№36",
+    name: "KanYe West",
+    img: kanye,
+    score: "19",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 34,
+    number: "№37",
+    name: "Dana White",
+    img: dana,
+    score: "18",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 35,
+    number: "№38",
+    name: "Tiger Woods",
+    img: tiger,
+    score: "12",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 36,
+    number: "№39",
+    name: "Tyson Fury ",
+    img: tyson,
+    score: "11",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 37,
+    number: "№40",
+    name: "Gervonta Davis ",
+    img: gervonta,
+    score: "5",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 39,
+    number: "№41",
+    name: "Vladimir Putin ",
+    img: vladimir,
+    score: "1",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 40,
+    number: "№42",
+    name: "Janet Yellen  ",
+    img: janet,
+    score: "1",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 41,
+    number: "№43",
+    name: "Christine Lagarde ",
+    img: christine,
+    score: "1",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
+  },
+  {
+    id: 42,
+    number: "№44",
+    name: "Prince Harry ",
+    img: harry,
+    score: "1",
+    address: "3BV2...76D723623",
+    dollar_price: "12.00",
   },
 ];
--- src/components/AiNFT/LeaderboardComponent/LeaderboardComponent.jsx
+++ src/components/AiNFT/LeaderboardComponent/LeaderboardComponent.jsx
@@ -1,11 +1,124 @@
 import React from "react";
 import Maintenance from "../../Maintenance/Maintenance";
+import { LeaderboardContainer } from "../AINFT.Style";
+import crown1 from "../../../assets/images/Ai-NFT/Leaderboard/crown-silver.png";
+import crown2 from "../../../assets/images/Ai-NFT/Leaderboard/crown-gold.png";
+import crown3 from "../../../assets/images/Ai-NFT/Leaderboard/crown-bronze.svg";
+import cristiano from "../../../assets/images/Ai-NFT/Leaderboard/Cristiano.svg";
+import lionel from "../../../assets/images/Ai-NFT/Leaderboard/lionel.svg";
+import kim from "../../../assets/images/Ai-NFT/Leaderboard/kim.png";
+import stick from "../../../assets/images/Ai-NFT/Leaderboard/stick.svg";
+import chainlink from "../../../assets/images/Ai-NFT/Leaderboard/chainlink.svg";
+import dollarYellow from "../../../assets/images/Ai-NFT/Leaderboard/dollar-yellow.svg";
+import { Leaderboard } from "../AiNFTData";
 
 const LeaderboardComponent = () => {
   return (
-    <>
-      <Maintenance />
-    </>
+    <LeaderboardContainer>
+      <div className="head">
+        <div className="box">
+          <div className="crown">
+            <img src={crown1} alt="crown" />
+            <span>2</span>
+          </div>
+          <div className="card">
+            <img src={lionel} alt="lionel" />
+            <div className="title">
+              <span>Lionel Messi</span>
+              <span id="purple">CryptoAi Score: 560</span>
+            </div>
+            <div className="id">
+              <span>
+                3BV2...76D723623
+                <img src={stick} alt="stick" />
+              </span>
+              <img src={chainlink} alt="chainlink" />
+            </div>
+            <div className="score">
+              {" "}
+              <img src={dollarYellow} alt="icon" />
+              <span>12.00</span>{" "}
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="crown">
+            <img src={crown2} alt="crown" />
+            <span>1</span>
+          </div>
+          <div className="card1">
+            <img src={cristiano} alt="cristiano" />
+            <div className="title">
+              <span>Cristiano Ronaldo</span>
+              <span id="purple">CryptoAi Score: 833</span>
+            </div>
+            <div className="id">
+              <span>
+                cee32...76D723623
+                <img src={stick} alt="stick" />
+              </span>
+              <img src={chainlink} alt="chainlink" />
+            </div>
+            <div className="score">
+              {" "}
+              <img src={dollarYellow} alt="icon" />
+              <span>12.00</span>{" "}
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="crown">
+            <img src={crown3} alt="crown" />
+            <span>3</span>
+          </div>
+          <div className="card">
+            <img src={kim} alt="kim" />
+            <div className="title">
+              <span>Kim Kardashian</span>
+              <span id="purple">CryptoAi Score: 461</span>
+            </div>
+            <div className="id">
+              <span>
+                3BV2...76D723623
+                <img src={stick} alt="stick" />
+              </span>
+              <img src={chainlink} alt="chainlink" />
+            </div>
+            <div className="score">
+              {" "}
+              <img src={dollarYellow} alt="icon" />
+              <span>14.40</span>{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main-body">
+        {Leaderboard.map((item) => (
+          <div className="rectangle">
+            <div className="icon">
+              <img src={item.img} alt="img" />
+              <span>{item.number}</span>
+            </div>
+            <div className="title">
+              <span>{item.name}</span>
+              <span id="purple">CryptoAi Score: {item.score}</span>
+              <div className="score">
+                {" "}
+                <img src={dollarYellow} alt="icon" />
+                <span>{item.dollar_price}</span>{" "}
+              </div>
+            </div>
+            <div className="id">
+              <span>
+                {item.address}
+                <img src={stick} alt="stick" />
+              </span>
+              <img src={chainlink} alt="chainlink" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </LeaderboardContainer>
   );
 };
 
